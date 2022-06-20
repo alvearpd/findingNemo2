d264fa7b77e33c468a3703e05d91b532f87b847c
Paul Alvear <pdalvear13@gmail.com>
2022-06-20 02:22:15 +0000

    Change from performance.now() to process.uptime() process.downtime() for this code to work.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const nemo = ['nemo'];
-
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'dquirt'];
+const large = new Array(100000).fill('nemo');
 function findNemo(array) {
-
+  let start_time = process.uptime();
+  //let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
       console.log('Found NEMO!');
     }
   }
+  let end_time = process.uptime();
+  //let t1 = performance.now();
+  //console.log('Call to find Nemo took' + (t1 - t0) + ' milliseconds');
+console.log("For loop took rgrgrgr ", end_time - start_time);
 
 }
 
-findNemo(nemo);
+findNemo(large);
